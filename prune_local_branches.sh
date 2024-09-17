642fd7dfd74146c0c335f2dc9da250d9c7fac92b
hopeaitchison <hope.aitchison1@gmail.com>
2024-09-17 18:23:50 +0100

    finishing off clean up script

--- prune_local_branches.sh
+++ prune_local_branches.sh
@@ -5,9 +5,16 @@ git fetch --prune
 # Before fetching, remove any remote-tracking references that no longer exist on the remote
 
 local_branches=$(git branch | sed 's/\*/ /')
-# get rid of the asterisk marking which branch is checked out
+# replace asterisk with a space
+# for consistency
 
+for branch in $local_branches; do
 
-for branch in local_branches; do
+    # checks if branch NOT present in remote with !
+    if ! git branch -r | grep "\sorigin/main$"; then
+    git branch -d $branch
+    echo "$branch did not exist in remote repo and has been deleted"
 
+    fi
+done
     
