8d45ed8cde2dc42eb783a7aaa41dadc0c70622a5
hopeaitchison <hope.aitchison1@gmail.com>
2024-09-19 16:43:41 +0100

    adding if statement to protect main branch

--- prune_local_branches.sh
+++ prune_local_branches.sh
@@ -4,17 +4,21 @@
 git fetch --prune
 # Before fetching, remove any remote-tracking references that no longer exist on the remote
 
-local_branches=$(git branch | sed 's/\*/ /')
-# replace asterisk with a space
+local_branches=$(git branch | sed 's/\*//')
+# removes asterisk
 # for consistency
 
 for branch in $local_branches; do
 
+    if [[ "$branch" == "main" ]]; then
+        echo "skipping main branch"
+        continue
+    fi
+
     # checks if branch NOT present in remote with !
     if ! git branch -r | grep "\s$branch/main$"; then
-    git branch -d $branch
-    echo "$branch did not exist in remote repo and has been deleted"
-
+        git branch -d $branch
+        echo "$branch did not exist in remote repo and has now been deleted"
     fi
 done
     
