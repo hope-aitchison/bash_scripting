8ebf815222dff24ea6f49c2b08884a9f7f4d565c
hopeaitchison <hope.aitchison1@gmail.com>
2024-09-19 17:14:22 +0100

    adding working branch protection

--- prune_local_branches.sh
+++ prune_local_branches.sh
@@ -8,15 +8,18 @@ local_branches=$(git branch | sed 's/\*//')
 # removes asterisk
 # for consistency
 
+current_branch=$(git branch --show-current)
+
 for branch in $local_branches; do
 
-    if [[ "$branch" == "main" ]]; then
-        echo "skipping main branch"
-        continue
+    # prevents main or working branch from being deleted
+    if [[ "$branch" == "main" || "$branch" == "$current_branch" ]]; then
+        echo "skipping $branch (local main or current working branch)"
+        continue # move to next iteration
     fi
 
     # checks if branch NOT present in remote with !
-    if ! git branch -r | grep "\s$branch/main$"; then
+    if ! git branch -r | grep -q "origin/$branch$"; then
         git branch -d $branch
         echo "$branch did not exist in remote repo and has now been deleted"
     fi
